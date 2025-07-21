127b1b87bc03059b5be07a799d90a32bb37301b1
gavin hemsada <69586429+GavinHemsada@users.noreply.github.com>
2025-07-21 19:31:10 +0530

    delete files

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./User.router";
-// export * from "./Review.routes";
-export * from "./Resturent.routes";
-// export * from "./Payment.routes";
-export * from "./OrderItem.routes";
-export * from "./Order.routes";
-export * from "./Food.routes";
-export * from "./Delivery.routes";
-export * from "./Customer.routes";
